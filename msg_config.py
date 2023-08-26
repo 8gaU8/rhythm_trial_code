4a1f894974e0d965ad3b302eac8ce3494aea3fc7
8gaU8 <haga.yuya2.2016@gmail.com>
2023-08-26 23:14:29 +0900

    音を1拍目とその他で変更

--- msg_config.py
+++ msg_config.py
@@ -4,51 +4,58 @@ from serial import Serial
 
 BASE_TIMES = 3
 
-from .message import (
-    Message,
-    play_none,
-    play_sound_trigger_factory,
-    play_trigger_factory,
-)
+from .dummy_serial import DummySerial
+from .message import Message, PlayFactories
 
 
-def build_base_msgs(port: Serial) -> List[Message]:
-    play_trigger_1 = play_trigger_factory(port, [1])
-    play_trigger_234 = play_trigger_factory(port, [2])
-    play_trigger_on_note = play_trigger_factory(port, [3])
-    play_sound_trigger = play_sound_trigger_factory(play_trigger_on_note)
+def build_base_msgs(
+    port: "Serial|DummySerial", pf: PlayFactories, base_time: float
+) -> List[Message]:
+    pt1 = pf.trig_fctr(port, [1])
+    pt234 = pf.trig_fctr(port, [2])
+
+    play_trig_on_note = pf.trig_fctr(port, [3])
+    note_h = pf.note_fctr(0)
+    note = pf.note_fctr(1)
+
+    pnth = pf.note_trig_fctr(note_h, play_trig_on_note)
+    pnt = pf.note_trig_fctr(note, play_trig_on_note)
+
+    t = lambda time: (time + base_time)
+
     msg_series = [
-        Message(play_sound_trigger, time=0),
-        Message(play_trigger_1, time=0),
-        Message(play_sound_trigger, time=0.375),
-        Message(play_trigger_234, time=0.5),
-        Message(play_sound_trigger, time=0.75),
-        Message(play_trigger_234, time=1.0),
-        Message(play_sound_trigger, time=1.25),
-        Message(play_sound_trigger, time=1.5),
-        Message(play_trigger_234, time=1.5),
-        Message(play_none, time=2.0),
+        Message(pnth, t(0)),
+        Message(pt1, t(0)),
+        Message(pnt, t(0.375)),
+        Message(pt234, t(0.5)),
+        Message(pnt, t(0.75)),
+        Message(pt234, t(1.0)),
+        Message(pnt, t(1.25)),
+        Message(pnt, t(1.5)),
+        Message(pt234, t(1.5)),
+        Message(pf.play_none, t(2.0)),
     ]
     return msg_series
 
 
-def build_trigger_msgs(port: Serial) -> List[Message]:
-    play_trigger_1 = play_trigger_factory(port, [1])
-    play_trigger_234 = play_trigger_factory(port, [2])
-    msg_series = [
-        Message(play_trigger_1, time=0),
-        Message(play_trigger_234, time=0.5),
-        Message(play_trigger_234, time=1.0),
-        Message(play_trigger_234, time=1.5),
-        Message(play_trigger_1, time=2.0),
-    ]
-    return msg_series
+def build_trigger_msgs(
+    port: "Serial|DummySerial", pf: PlayFactories, base_time: float, delay: float
+) -> List[Message]:
+    pt1 = pf.trig_fctr(port, [1])
+    pt234 = pf.trig_fctr(port, [2])
+    play_trig_on_note = pf.trig_fctr(port, [3])
+    pn = pf.note_fctr(0)
+    pnt = pf.note_trig_fctr(pn, play_trig_on_note)
 
+    t = lambda time: (time + base_time)
 
-def build_probe_tone(port: Serial) -> List[Message]:
-    play_trigger_on_note = play_trigger_factory(port, [3])
-    play_sound_trigger = play_sound_trigger_factory(play_trigger_on_note)
     msg_series = [
-        Message(play_sound_trigger, time=0.0),
+        Message(pnt, t(0)),
+        Message(pt1, t(0)),
+        Message(pt234, t(0.5)),
+        Message(pt234, t(1.0)),
+        Message(pt234, t(1.5)),
+        Message(pt1, t(2.0)),
+        Message(pnt, t(2.0) + delay),
     ]
     return msg_series
