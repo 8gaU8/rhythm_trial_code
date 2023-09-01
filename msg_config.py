d17e4a4f86eafb9ab94970ad0ead511665b355c6
8gaU8 <haga.yuya2.2016@gmail.com>
2023-09-01 17:25:16 +0900

    コメント追加、変数名変更

--- msg_config.py
+++ msg_config.py
@@ -18,13 +18,13 @@ def build_base_msgs(
     note_h = pf.note_fctr(0)
     note = pf.note_fctr(1)
 
-    pnth = pf.note_trig_fctr(note_h, play_trig_on_note)
+    pnt1 = pf.note_trig_fctr(note_h, play_trig_on_note)
     pnt = pf.note_trig_fctr(note, play_trig_on_note)
 
     t = lambda time: (time + base_time)
 
     msg_series = [
-        Message(pnth, t(0)),
+        Message(pnt1, t(0)),
         Message(pt1, t(0)),
         Message(pnt, t(0.375)),
         Message(pt234, t(0.5)),
@@ -56,6 +56,8 @@ def build_trigger_msgs(
         Message(pt234, t(1.0)),
         Message(pt234, t(1.5)),
         Message(pt1, t(2.0)),
-        Message(pnt, t(2.0) + delay),
+        # BPM120がベースになっているため、delayは半分にする。
+        # たとえば、delay = 0.2のときBPM120では1拍の20パーセント、つまり 0.5sec * 0.2 秒だけ遅れてほしい
+        Message(pnt, t(2.0) + delay / 2),
     ]
     return msg_series
